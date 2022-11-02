f143edd0bfaa612f1760c84dfc58077403f64caa
Bohndiggin <bohndiggin@gmail.com>
2022-11-02 12:02:43 -0600

    finished demo

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const { home } = require('./controller')
+
+app.get('/', home)
+
 const {PORT} = process.env
 
 app.listen(PORT, () => {console.log(`listening on ${PORT}`)})
